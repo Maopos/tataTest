19f6473842ba5fc3871279f70cb10580a1c9af30
Mauricio Posada <mauricioposadamurillo@gmail.com>
2025-10-01 08:53:54 -0500

    Initial Commit

---
+++ TataTest/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TataTest
+//
+//  Created by Mauricio Posada on 1/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
